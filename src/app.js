0983614a47340cd6867869cc0a1270d31b9b4c55
Allen Tsai <allentsai93@gmail.com>
2017-12-19 01:41:12 -0800

    Added login screen

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+  componentWillMount() {
+      firebase.initializeApp({
+    apiKey: 'AIzaSyBmwtTVLXoPr3FiAqellsG01Zi9tR78LuQ',
+    authDomain: 'authentication-ec0b0.firebaseapp.com',
+    databaseURL: 'https://authentication-ec0b0.firebaseio.com',
+    projectId: 'authentication-ec0b0',
+    storageBucket: 'authentication-ec0b0.appspot.com',
+    messagingSenderId: '572333544823'
+  });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An app</Text>
+        <LoginForm />
       </View>
     );
   }
